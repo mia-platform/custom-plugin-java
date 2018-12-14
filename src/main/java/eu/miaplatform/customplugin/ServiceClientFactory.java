63649f2344076ba2dec1a254943e674c83aca2eb
antonio.lasalandra <antonio.lasalandra@mia-platform.eu>
2018-12-14 10:57:16 +0100

    Fix

--- src/main/java/eu/miaplatform/customplugin/ServiceClientFactory.java
+++ src/main/java/eu/miaplatform/customplugin/ServiceClientFactory.java
@@ -2,7 +2,7 @@ package eu.miaplatform.customplugin;
 
 public class ServiceClientFactory {
 
-    static ServiceClient getCRUDServiceClient(String apiPath, String apiSecret, CustomPluginHeadersPropagator headersPropagator) {
+    public static ServiceClient getCRUDServiceClient(String apiPath, String apiSecret, CustomPluginHeadersPropagator headersPropagator) {
         return new CRUDServiceClient(apiPath, apiSecret, headersPropagator);
     }
 }
