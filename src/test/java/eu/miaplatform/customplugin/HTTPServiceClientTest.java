e6f61351676e1affd9403744b2c0e4c01a555c75
Grieco <grieco@intre.it>
2020-07-03 15:14:46 +0200

    Test get with mock server

--- src/test/java/eu/miaplatform/customplugin/HTTPServiceClientTest.java
+++ src/test/java/eu/miaplatform/customplugin/HTTPServiceClientTest.java
@@ -1,17 +1,47 @@
 package eu.miaplatform.customplugin;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import eu.miaplatform.service.*;
 import okhttp3.Headers;
 import okhttp3.Response;
+import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.junit.Assert.assertNotNull;
+
 public class HTTPServiceClientTest {
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(3000);
+
+    @Test
+    public void testGet() throws IOException {
+        String url = "/test?foo=bar";
+        String responseBody = "{\"result\":\"ok\"}";
+        stubFor(WireMock.get(urlEqualTo(url))
+                .withQueryParam("foo", WireMock.equalTo("bar"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBody(responseBody)));
+
+        JsonObject obj = new JsonObject();
+        Service service = new Service("localhost", obj, new InitServiceOptions(3000, Protocol.HTTP, obj, ""));
+        Response response = service.get("test", "foo=bar", new ServiceOptions());
+
+        assertEquals(response.code(), 200);
+        assertNotNull(response.body());
+        assertEquals(response.body().string(), responseBody);
+        verify(getRequestedFor(urlEqualTo(url))
+                .withQueryParam("foo", WireMock.equalTo("bar")));
+    }
+
     @Test
     public void parseHeaders() {
         JsonObject obj = new JsonObject();
