97e608bb47d477170104460f374fa1aa6f95a675
Grieco <grieco@intre.it>
2020-07-08 16:31:21 +0200

    Use builder for InitServiceOptions

--- src/main/java/eu/miaplatform/service/InitServiceOptions.java
+++ src/main/java/eu/miaplatform/service/InitServiceOptions.java
@@ -1,8 +1,13 @@
 package eu.miaplatform.service;
 
+import lombok.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Data
+@Builder
+@AllArgsConstructor
 public class InitServiceOptions {
     private int port;
     private Protocol protocol;
@@ -14,44 +19,5 @@ public class InitServiceOptions {
         this.protocol = Protocol.HTTP;
         this.headers = new HashMap<>();
         this.prefix = "";
-    }
-
-    public InitServiceOptions(int port, Protocol protocol, Map<String, String> headers, String prefix) {
-        this.port = port;
-        this.protocol = protocol;
-        this.headers = headers;
-        this.prefix = prefix;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
-    public void setPort(int port) {
-        this.port = port;
-    }
-
-    public Protocol getProtocol() {
-        return protocol;
-    }
-
-    public void setProtocol(Protocol protocol) {
-        this.protocol = protocol;
-    }
-
-    public Map<String, String> getHeaders() {
-        return headers;
-    }
-
-    public void setHeaders(Map<String, String> headers) {
-        this.headers = headers;
-    }
-
-    public String getPrefix() {
-        return prefix;
-    }
-
-    public void setPrefix(String prefix) {
-        this.prefix = prefix;
     }
 }
--- src/main/java/eu/miaplatform/service/ServiceOptions.java
+++ src/main/java/eu/miaplatform/service/ServiceOptions.java
@@ -1,7 +1,9 @@
 package eu.miaplatform.service;
 
-import java.util.Map;
+import lombok.*;
 
+@Data
+@AllArgsConstructor
 public class ServiceOptions extends InitServiceOptions {
     private ReturnAs returnAs;
     private int[] allowedStatusCodes;
@@ -12,28 +14,5 @@ public class ServiceOptions extends InitServiceOptions {
         this.returnAs = ReturnAs.JSON;
         this.allowedStatusCodes = new int[]{200, 201, 202};
         this.isMiaHeaderInjected = true;
-    }
-
-    public ServiceOptions(int port, Protocol protocol, Map<String, String> header, String prefix) {
-        super(port, protocol, header, prefix);
-    }
-
-    public ServiceOptions(int port, Protocol protocol, Map<String, String> header, String prefix, ReturnAs returnAs, int[] allowedStatusCodes, boolean isMiaHeaderInjected) {
-        super(port, protocol, header, prefix);
-        this.returnAs = returnAs;
-        this.allowedStatusCodes = allowedStatusCodes;
-        this.isMiaHeaderInjected = isMiaHeaderInjected;
-    }
-
-    public ReturnAs getReturnAs() {
-        return returnAs;
-    }
-
-    public int[] getAllowedStatusCodes() {
-        return allowedStatusCodes;
-    }
-
-    public boolean isMiaHeaderInjected() {
-        return isMiaHeaderInjected;
     }
 }
