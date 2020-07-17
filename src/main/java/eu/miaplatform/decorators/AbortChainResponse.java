019e34e2b938a620f9a38c81ca4cac75903ba5a3
Grieco <grieco@intre.it>
2020-07-17 18:43:06 +0200

    Fix abort chain respons construction

--- src/main/java/eu/miaplatform/decorators/AbortChainResponse.java
+++ src/main/java/eu/miaplatform/decorators/AbortChainResponse.java
@@ -8,10 +8,7 @@ import static eu.miaplatform.decorators.constants.DecoratorConstants.ABORT_CHAIN
 
 @Data
 public class AbortChainResponse extends DecoratorResponse{
-    private int statusCode;
-
     public AbortChainResponse(Object finalBody, Map<String, String> finalHeaders) {
         super(ABORT_CHAIN_STATUS_CODE, finalHeaders, finalBody);
-        this.statusCode = statusCode;
     }
 }
